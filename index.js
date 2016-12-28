ff16719205507dfa30a860f0704861222fa8c1f4
dmclough <davidclough@gmail.com>
2016-12-28 12:14:35 -0800

    committing with working favorites displayed on pokemon page

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ var request = require('request');
 var bodyParser = require('body-parser');
 var ejsLayouts = require('express-ejs-layouts');
 var app = express();
+// attempting to define db START
+var db = require("./models");
+// attempting to define db END
+
 
 app.use(require('morgan')('dev'));
 app.set('view engine', 'ejs');
@@ -10,13 +14,26 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ejsLayouts);
 
 app.get('/', function(req, res) {
+  // var pokemonUrl = '/public/json/poke850.json'; DOES NOT WORK
   var pokemonUrl = 'http://pokeapi.co/api/v2/pokemon/';
+
 
   request(pokemonUrl, function(error, response, body) {
     var pokemon = JSON.parse(body).results;
     res.render('index', { pokemon: pokemon });
   });
 });
+
+//START POKEMONS
+app.get("/pokemon", function(req,res){
+  db.pokemon.findAll().then(function(bar) {
+      res.render("pokemon", {"foo": bar});
+  });
+});
+//END POKEMONS
+
+
+
 
 app.use('/pokemon', require('./routes/pokemon'));
 
